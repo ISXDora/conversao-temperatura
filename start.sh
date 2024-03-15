4ae0dd3af6d16ec91005bf9ed644c25361e25224
Isadora Xavier <isinhasena@gmail.com>
2024-03-14 22:33:24 -0300

    adiiconando arquivo de execução do container

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Esse script executará uma imagem do node.js que roda uma aplicação de conversão de temperatura
+#Autor: Isadora Xavier | E-mail: isadoraxavier@outlook.com
+
+docker container run -p 8080:8080 -d ubuntu-conversao-temp
